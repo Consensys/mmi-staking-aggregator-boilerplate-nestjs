7bd14d785dc79dbc5b20b1d700302c43f2c44e5a
Xavier Brochard <xavier.brochard@consensys.net>
2023-02-24 18:48:05 +0700

    chore: lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,11 @@ import { ScheduleModule } from '@nestjs/schedule'
 import { TerminusModule } from '@nestjs/terminus'
 import config, { validationSchema } from './config/configuration'
 
-import { HealthController } from './health/health.controller'
 import { AuthModule } from './auth/auth.module'
-import { StakesModule } from './stakes/stakes.module'
 import { DetailsModule } from './details/details.module'
-import { WithdrawalsModule } from './withdrawals/withdrawals.module';
+import { HealthController } from './health/health.controller'
+import { StakesModule } from './stakes/stakes.module'
+import { WithdrawalsModule } from './withdrawals/withdrawals.module'
 
 @Module({
   imports: [
